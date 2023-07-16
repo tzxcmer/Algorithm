c9f78aba88e9c8a1958d7e838513f08db9ad0962
tzxcmer <94823741+tzxcmer@users.noreply.github.com>
2023-07-16 10:16:11 +0800

    山脉数组的峰顶索引

---
+++ BinarySearch/Peak Index in a Mountain Array/LeetCode852.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int l = 1,r = n-2;
+        while(l < r)
+        {
+            int mid = (r-l+1)/2+l;
+            if(arr[mid] > arr[mid-1])
+                l = mid;
+            else
+                r = mid-1;
+        }
+
+        return l;
+    }
+};
