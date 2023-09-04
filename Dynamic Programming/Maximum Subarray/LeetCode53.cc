672cc2fab1a3ff8cc460d1e7fc739b3cf960bc37
tzxcmer <94823741+tzxcmer@users.noreply.github.com>
2023-09-04 23:16:32 +0800

    最大子数组和

---
+++ Dynamic Programming/Maximum Subarray/LeetCode53.cc
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        int res = nums[0];
+        for(int i=1;i<n;i++)
+        {
+            dp[i] = max(dp[i-1] + nums[i],nums[i]);
+            res = max(res,dp[i]);
+        }
+
+        return res;
+    }
+};
