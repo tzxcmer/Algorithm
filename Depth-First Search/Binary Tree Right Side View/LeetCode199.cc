7791f7b1ad4887319cdbd1be6b9d4d78a0b785c5
唐梓迅 <94823741+tzxcmer@users.noreply.github.com>
2023-09-28 22:56:23 +0800

    二叉树的右视图

---
+++ Depth-First Search/Binary Tree Right Side View/LeetCode199.cc
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        dfs(root,0,ans);
+        return ans;
+    }
+    void dfs(TreeNode* root, int depth, vector<int>& ans)
+    {
+        if(!root) return ;
+        if(depth == ans.size()) ans.push_back(root->val);
+        dfs(root->right,++depth,ans);
+        dfs(root->left,depth,ans);
+        return ;
+    }
+};
