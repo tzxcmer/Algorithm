926f0d7636dcf69fabe97913ca1d9f8a9d05862a
唐梓迅 <94823741+tzxcmer@users.noreply.github.com>
2023-11-01 22:50:37 +0800

    超级次方

---
+++ Recurrent/Super Pow/LeetCode372.cc
@@ -0,0 +1,25 @@
+class Solution {
+    const int MOD = 1337;
+
+    int pow(int x, int n) {
+        int res = 1;
+        while (n) {
+            if (n % 2) {
+                res = (long) res * x % MOD;
+            }
+            x = (long) x * x % MOD;
+            n /= 2;
+        }
+        return res;
+    }
+
+public:
+    int superPow(int a, vector<int> &b) {
+        int ans = 1;
+        for (int i = b.size() - 1; i >= 0; --i) {
+            ans = (long) ans * pow(a, b[i]) % MOD;
+            a = pow(a, 10);
+        }
+        return ans;
+    }
+};
