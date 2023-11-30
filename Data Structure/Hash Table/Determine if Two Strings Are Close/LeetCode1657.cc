9673d41f233b272f7555aac5f2a4e6bab093d3f8
唐梓迅 <94823741+tzxcmer@users.noreply.github.com>
2023-11-30 22:13:28 +0800

    确定两个字符串是否接近

---
+++ Data Structure/Hash Table/Determine if Two Strings Are Close/LeetCode1657.cc
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        vector<int> cnt1(26),cnt2(26);
+        for(auto ch : word1)
+            cnt1[ch-'a']++;
+        for(auto ch : word2)
+            cnt2[ch-'a']++;
+        for(int i=0;i<26;i++)
+        {
+            if(cnt1[i] == 0 && cnt2[i] > 0 || 
+            cnt2[i] == 0 && cnt1[i] > 0)
+                return false;
+        }
+        sort(cnt1.begin(),cnt1.end());
+        sort(cnt2.begin(),cnt2.end());
+
+        return cnt1 == cnt2;
+    }
+};
